4a0f4e81ba031c7410435f2431f85601fca5ce58
Petros Xenofontos <petros.x12@gmail.com>
2020-02-09 06:06:47 +0000

    Fixed firebase database

--- scripts/firebase_functions.js
+++ scripts/firebase_functions.js
@@ -1,3 +1,5 @@
+// your page initialization code here
+// the DOM will be available here
 const firebaseConfig = {
     apiKey: "AIzaSyDQ3gaHvcFpEkusY-VAS2sgUru_terdUVA",
     authDomain: "hitmeservermate.firebaseapp.com",
@@ -9,17 +11,17 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
-var defaultDatabase = firebase.database();
+defaultDatabase = firebase.database();
 
 //Get available moves once
 var moveRef = defaultDatabase.ref("moves/");
-moveRef.once("value", function(data) {
+moveRef.once("value", function (data) {
     var values = data.val();
     console.log(typeof values);
     console.log(values);
 
     for (var property in values) {
-        console.log(Object.getOwnPropertyDescriptor(values,property).value.damage);
+        console.log(Object.getOwnPropertyDescriptor(values, property).value.damage);
     }
 
 }, function (error) {
@@ -35,53 +37,54 @@ CODE THAT GETS POSE OF PLAYER B
 
 //Subscription to notify when number of moves changes
 var playerMoves = defaultDatabase.ref("moveNo/");
-playerMoves.on("value", function(data){
+playerMoves.on("value", function (data) {
     var moveNumber = data.val();
-    console.log("Number of moves: "+moveNumber.moveNumber);
-}, function(error){
-    console.log("Error: "+error.code);
+    console.log("Number of moves: " + moveNumber.moveNumber);
+}, function (error) {
+    console.log("Error: " + error.code);
 })
 
 //Get health value of player A
 var playerA = defaultDatabase.ref("players/A");
-playerA.on("value", function(data){
+playerA.on("value", function (data) {
     var health = data.val();
-    console.log("Health player A: "+health.health);
-}, function(error){
-    console.log("Error: "+ error.code);
+    console.log("Health player A: " + health.health);
+}, function (error) {
+    console.log("Error: " + error.code);
 });
 
 //Get health value of player B
 var playerB = defaultDatabase.ref("players/B");
-playerB.on("value", function(data){
+playerB.on("value", function (data) {
     var health = data.val();
-    console.log("Health player B: "+health.health);
-}, function(error){
-    console.log("Error: "+ error.code);
+    console.log("Health player B: " + health.health);
+}, function (error) {
+    console.log("Error: " + error.code);
 });
 
-function updateHealthPlayerA(healthValue){
+function updateHealthPlayerA(healthValue) {
     console.log("Updating value");
 
     playerA.update(
         {
-            "health":healthValue
+            "health": healthValue
         }
     )
 }
 
-function updateHealthPlayerB(healthValue){
+function updateHealthPlayerB(healthValue) {
     console.log("Updating value");
 
     playerB.update(
         {
-            "health":healthValue
+            "health": healthValue
         }
     )
 }
 
 function reset_game() {
-    defaultDatabase.ref('/moveNo').update({'moveNumber' : 0});
-    defaultDatabase.ref('/players/A').update({'health':100});
-    defaultDatabase.ref('/players/B').update({'health':100});
+    defaultDatabase.ref('/moveNo').update({ 'moveNumber': 0 });
+    defaultDatabase.ref('/players/A').update({ 'health': 100 });
+    defaultDatabase.ref('/players/B').update({ 'health': 100 });
 }
+
